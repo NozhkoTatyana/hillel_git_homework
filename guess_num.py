edf89fe30b68fd244948ae604d7f2c457e18532a
NozhkoTatyana <tatyana.nozhko@ukr.net>
2023-07-10 22:33:35 +0300

    first commit

--- guess_num.py
+++ guess_num.py
@@ -14,21 +14,6 @@ print('Введіть число: ')
 num_user = input()
 print(f'Ваше число: {num_user}')
 
-
-def check_next_previous(num_user):
-    num = abs(int(num_user) - num_bot)
-
-    if (int(num_user) < num_bot or int(num_user) > num_bot) and num != 1 or\
-     (int(num_user) < 1 or int(num_user) > 10):
-        print(answer_error)
-
-    elif int(num_user) == num_bot:
-        print('ВІТАЮ, ВИ ЦЕ ЗРОБИЛИ')
-
-    else:
-        print("ВИ БУЛИ МАКСИМАЛЬНО БЛИЗЬКО")
-
-
 if num_user.isdigit():
     check_next_previous(num_user)
 
